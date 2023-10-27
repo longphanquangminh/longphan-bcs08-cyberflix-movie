9c936b2f44febae7f41dce63615bebc59d0e40fe
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-10-27 21:22:01 +0700

    up

--- src/template/PrivateRoute.jsx
+++ src/template/PrivateRoute.jsx
@@ -1,11 +1,21 @@
+import { message } from "antd";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
   const { info } = useSelector(state => {
     return state.adminReducer;
   });
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (info?.maLoaiNguoiDung !== "QuanTri") {
+      message.error("Please login first, admin!");
+      navigate("/admin/auth");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   if (info?.maLoaiNguoiDung === "QuanTri") {
     return children;
   }
-  window.location.href = "/admin/auth";
 }
