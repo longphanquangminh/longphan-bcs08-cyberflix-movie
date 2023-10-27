ddd7cdcd223b6f568b8097956916bc984f144e6f
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-10-27 21:15:20 +0700

    u

--- src/template/PrivateRoute.jsx
+++ src/template/PrivateRoute.jsx
@@ -1,9 +1,5 @@
 import { useSelector } from "react-redux";
 
-// PrivateRoute.propTypes = {
-//   children: PropTypes.node.isRequired,
-// };
-
 export default function PrivateRoute({ children }) {
   const { info } = useSelector(state => {
     return state.adminReducer;
@@ -12,5 +8,4 @@ export default function PrivateRoute({ children }) {
     return children;
   }
   window.location.href = "/admin/auth";
-  //   return children;
 }
