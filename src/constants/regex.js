46f05f4c277ee478ab2ce321155796c629a4a103
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-10-28 21:35:10 +0700

    up

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -3,4 +3,5 @@ export const trailerUrlRegex =
 export const trailerYoutube = /youtu\.be\/([A-Za-z0-9_-]+)/;
 export const imageUrlRegex = /\.(jpg|jpeg|png|gif|bmp)$/i;
 export const positiveNumberRegex = /^[+]?((0*[1-9]+\d*)|(\d*\.\d+([eE][-+]?\d+)?))$/;
-export const priceRegex = /^(?:75000|7[5-9]\d{3}|[89]\d{4}|1\d{5}|200000)$/;
+export const unitedStatesPriceRegex = /^(75000(\.0+)?|7[5-9]\d{3}(\.\d+)?|[89]\d{4}(\.\d+)?|1\d{5}(\.\d+)?|200000)$/;
+export const priceRegex = /^(75000(,0+)?|7[5-9]\d{3}(,\d+)?|[89]\d{4}(,\d+)?|1\d{5}(,\d+)?|200000)$/;
