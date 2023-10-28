789f8e42d25b041ed8670f571f9d62440b444da0
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-10-28 14:19:15 +0700

    up

--- src/component/PlayVideo.jsx
+++ src/component/PlayVideo.jsx
@@ -2,8 +2,9 @@ import { PlayCircle } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { chooseTrailer } from "../redux/action/user";
 import { trailerUrlRegex } from "../constants/regex";
+import { defaultTrailer } from "../constants/defaultValues";
 
-export default function PlayVideo({ isCard, trailer = "https://www.youtube.com/watch?v=kvAfmYNtugQ" }) {
+export default function PlayVideo({ isCard, trailer = defaultTrailer }) {
   const dispatch = useDispatch();
   const handleChooseTrailer = trailer => {
     const url = new URL(trailer);
@@ -15,7 +16,7 @@ export default function PlayVideo({ isCard, trailer = "https://www.youtube.com/w
       className={`absolute hidden group-hover:block duration-300 ${
         isCard ? `top-1/4` : `top-1/2`
       } text-white left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer mx-auto z-40`}
-      onClick={() => handleChooseTrailer(trailerUrlRegex.test(trailer) ? trailer : "https://www.youtube.com/watch?v=kvAfmYNtugQ")}
+      onClick={() => handleChooseTrailer(trailerUrlRegex.test(trailer) ? trailer : defaultTrailer)}
     >
       <PlayCircle size={52} />
     </div>
